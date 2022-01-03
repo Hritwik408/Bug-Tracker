e8b7b8ac9c284eaf1d4027c35861e84b00547b35
HRITWIK VERMA <66999270+Hritwik408@users.noreply.github.com>
2022-01-03 22:38:43 +0530

    sidecards entry

---
+++ src/Views/sidebar/sidebar.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class SiderBar extends Component {
+  state = {};
+  render() {
+    return <div className="sidebar-wrapper">"side bar"</div>;
+  }
+}
+
+export default SiderBar;
