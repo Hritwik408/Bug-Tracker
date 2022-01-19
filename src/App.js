ee8143e68dcb8cb29287759252caaf81aeb7f7f9
HRITWIK VERMA <66999270+Hritwik408@users.noreply.github.com>
2022-01-19 23:26:12 +0530

    dashboard

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Sidebar from "./Views/sidebar/sidebar";
 import viewBugPage from "./Views/Pages/viewbugs";
 import createBug from "./Views/bugcreate/edit/bugForm";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import dashboard from "./Views/Pages/Dashboard/dashboard";
 
 function App() {
   const { auth } = useSelector((state) => state);
@@ -16,6 +17,9 @@ function App() {
         <>
           <Sidebar />
           <Switch>
+            <Route path="/" exact>
+              <dashboard />
+            </Route>
             <Route path="/viewbugs">
               <viewBugPage />
             </Route>
